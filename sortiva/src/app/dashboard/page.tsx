b3aab7ebadf5966d491f6db3ca4a594d92da4b0e
shadowrager_85 <princmartins2111@gmail.com>
2025-06-07 12:25:31 +0300

    v4

--- sortiva/src/app/dashboard/page.tsx
+++ sortiva/src/app/dashboard/page.tsx
@@ -7,9 +7,34 @@ return (
 <div>
     <h1 className="text-4xl font-semibold">Dashboard</h1>
     <p className="mb-4">Welcome to your dashboard</p>
-    
 </div>
+<div className="flex flex-row space-x-4 mb-6">
+    <div className="card border-2 rounded-2xl p-4 flex-1">
+        <h1 className="font-bold text-2xl">Total Waste Sorted</h1>
+        <p className="text-2xl font-bold">0 kg</p>
+    </div>
+    <div className="card border-2 rounded-2xl p-4 flex-1">
+        <h1 className="font-bold text-2xl">Total Waste Sorted</h1>
+        <p className="text-2xl font-bold">0 kg</p>
+    </div>
+    <div className="card border-2 rounded-2xl p-4 flex-1">
+        <h1 className="font-bold text-2xl">Total Waste Sorted</h1>
+        <p className="text-2xl font-bold">0 kg</p>
+    </div>
+</div>
+<div className="Quick Actions mb-6">
+    <h1 className="mb-2">Quick Actions</h1>
+    <div className="flex flex-row space-x-4">
+                <div className="btn flex flex-col flex-1 bg-emerald-400 w-96 h-48 rounded-2xl p-10 m-2 items-center justify-center cursor-pointer hover:scale-105 shadow-2xl transition-all duration-300 hover:shadow-amber-800 ">
+            <div className="icon mb-2"></div>
+            <h1 className="text-2xl font-bold">Scan Waste</h1>
+        </div>
+                <div className="btn flex flex-col flex-1 bg-emerald-400 w-96 h-48 rounded-2xl p-10 m-2 items-center justify-center cursor-pointer hover:scale-105 shadow-2xl transition-all duration-300 hover:shadow-amber-800 ">
+            <div className="icon mb-2"></div>
+            <h1 className="text-2xl font-bold">Learn And Earn</h1>
+        </div>
+    </div>
+    </div>
 </>
 );
 }
-//"inferno24k or rexinvex"
