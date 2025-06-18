b8eee8892d845d50a9b8c0ce92ca002e178c9c51
shadowrager_85 <princmartins2111@gmail.com>
2025-06-18 17:00:24 +0300

    backend v1

---
+++ sortiva/src/lib/mongodb.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+
+const MONGODB_URL = process.env.DATABASE_URL as string;
+
+if (!MONGODB_URL) {
+  throw new Error('Please define the DATABASE_URL environment variable inside .env.local');
+}
+ async function connectToDatabase() {
+    if (mongoose.connection.readyState >= 1) {
+        return mongoose;
+    }
+    const opts = {
+        bufferCommands: false,
+    }
+    await mongoose.connect(MONGODB_URL, opts);
+    return mongoose;
+}
+export default connectToDatabase;
